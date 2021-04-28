2c35c43f921b084c2bc0efc45a64524032c7bef8
Maxim Safioulline <maxim@connectedcatmedia.com>
2021-04-27 20:51:16 -0700

    in class example

--- week10/in_class/script.js
+++ week10/in_class/script.js
@@ -1,74 +1,36 @@
-let field = document.getElementById("colorField");
-let colorBTN = document.getElementById("colorToggle");
+let header = document.getElementById("myHeader");
+let clrBTN = document.getElementById("colorBTN");
+let quokkaImg = document.getElementById("quokkaImg");
+let tgglBTN = document.getElementById("toggleBTN");
 
-let toggleIMG = document.getElementById("quokkaIMG");
-let imageBTN = document.getElementById("imgToggle");
+function changeTheColor(){
+    // WE'LL DO SOME COLOR CHANGING HERE
+    let redComp = Math.random() * 255;
+    let greenComp = Math.random() * 255;
+    let blueComp = Math.random() * 255;
+    console.log("RED: " + redComp);
+    console.log("GREEN: " + greenComp);
+    console.log("BLUE: " + blueComp);
 
-let catSelect = document.getElementById("category");
-let subSelect = document.getElementById("subcategory");
+    header.style.backgroundColor = "rgb("+ redComp + ", "+ greenComp +", " + blueComp + ")";
+}
 
-let imageToggle = function(){
+let toggleTheImage = function(){
+    // WE'LL DO SOME IMAGE TOGGLING HERE
 
-    if(toggleIMG.alt === "Qute quokka 1"){
-        //do stuff
-        toggleIMG.src = "../what_JS_can_do/quokka2.jpg";
-        toggleIMG.alt = "Qute quokka 2";
+    if(tgglBTN.innerText === "Show the second one!") {
+        console.log("show the second image");
+        quokkaImg.src = "quokka2.jpg";
+        tgglBTN.innerText = "Now show the first one!";
     }
     else {
-        //do other stuff
-       toggleIMG.src = "../what_JS_can_do/quokka1.jpg";
-       toggleIMG.alt = "Qute quokka 1";
+        console.log("show the first image");
+        quokkaImg.src = "quokka1.jpg";
+        tgglBTN.innerText = "Show the second one!";
     }
 }
 
-let categoryUpdate = function(){
-    let foodSubcats = ["coffee", "desserts", "cocktails", "muffins"];
-    let animSubcats = ["kittens", "puppies", "quokkas"];
-    let natureSubcats = ["forests", "mountains", "lakes"];
+changeTheColor();
 
-    subSelect.innerHTML = "";
-
-    if(catSelect.value === "food"){
-        console.log("food");
-        foodSubcats.forEach(function(elem){
-            let opt = document.createElement("option");
-            opt.value = elem;
-            opt.innerText = elem;
-            subSelect.appendChild(opt);
-        })
-    }
-    else if(catSelect.value === "anim"){
-        console.log("animals");
-        animSubcats.forEach(function(elem){
-            let opt = document.createElement("option");
-            opt.value = elem;
-            opt.innerText = elem;
-            subSelect.appendChild(opt);
-        })
-    }
-    else if(catSelect.value === "nat"){
-        console.log("nature");
-        natureSubcats.forEach(function(elem){
-            let opt = document.createElement("option");
-            opt.value = elem;
-            opt.innerText = elem;
-            subSelect.appendChild(opt);
-        })
-    }
-}
-
-//imageToggle();
-
-catSelect.addEventListener("change", categoryUpdate);
-
-imageBTN.addEventListener("click", imageToggle);
-
-colorBTN.addEventListener("click", function(){
-    console.log("Hello " + "World");
-
-    let rComp = Math.random()*255;
-    let gComp = Math.random()*255;
-    let bComp = Math.random()*255;
-    //"rgb(18, 187, 56)"
-    field.style.backgroundColor = "rgb(" + rComp + ", " + gComp + ", " + bComp + ")";
-})
+clrBTN.addEventListener("click", changeTheColor);
+tgglBTN.addEventListener("click", toggleTheImage);
